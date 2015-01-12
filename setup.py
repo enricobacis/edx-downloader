22a8ffbb44befb917ca6b2d743b32209ce684abb
enricobacis <enrico.bacis@gmail.com>
2015-01-12 12:11:40 +0100

    Add distutils setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import shutil
+
+# rename script
+shutil.copyfile('edx-dl.py', 'build/edx-downloader')
+
+setup(
+    name='edx-downloader',
+    version='0.1',
+    url='https://github.com/shk3/edx-downloader',
+    description='A simple tool to download video lectures from edx.org.',
+    long_description=open('README.md').read(),
+    author='shk3',
+    author_email='shk3@users.noreply.github.com',
+    maintainer='enricobacis',
+    maintainer_email='enrico.bacis@gmail.com',
+    requires=['youtube_dl', 'beautifulsoup4'],
+    scripts=['build/edx-downloader'],
+)
